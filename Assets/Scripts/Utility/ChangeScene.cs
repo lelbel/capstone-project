e36a36d68cf93870611c3e6f6faaea3be9d65719
Lily Elbel <lelbel@uncc.edu>
2025-11-13 16:45:58 -0500

    eventsystemmanager script goes into the garbage
    i just needed to disable the event system from the canvas prefab in the map scene. i hate it here

--- Assets/Scripts/Utility/ChangeScene.cs
+++ Assets/Scripts/Utility/ChangeScene.cs
@@ -1,7 +1,7 @@
-    using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Navigate : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     [SerializeField] private string scene;
 
